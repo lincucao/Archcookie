8b0366a7b6a1a2152fb5c6b0adddd70cd1f2fe12
lincucao <linzhixiong23@163.com>
2020-05-18 21:43:39 +0800

    修复菜单逻辑

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 $(function(){
-
     // 左侧菜单收缩
-    $("#menuToggle").on("click", function(){
+    $("#menuToggle").on("click", function () {
       $("#sidebar").toggleClass("sidebar-active");
       $("#mainContent").toggleClass("main-active");
     });
     $("#sidebar ul li a").on("click", function () {
       $("#sidebar").toggleClass("sidebar-active");
     });
+});      if ($("#sidebar:has('.sidebar-active')"))
+      $("#sidebar").toggleClass("sidebar-active");
+    })
 });
+
