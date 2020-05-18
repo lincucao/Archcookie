4b9029c21c464df96897324be23a621a398a439a
lincucao <linzhixiong23@163.com>
2020-05-18 22:05:25 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,11 +4,26 @@ $(function(){
       $("#sidebar").toggleClass("sidebar-active");
       $("#mainContent").toggleClass("main-active");
     });
-    $("#sidebar ul li a").on("click", function () {
+    // 点击菜单隐藏
+    $(".side-menu ul li a").on("click", function () {
       $("#sidebar").toggleClass("sidebar-active");
     });
-});      if ($("#sidebar:has('.sidebar-active')"))
+    // 点击空白处隐藏
+    $(".hideSidebar").on("click", function () {
+      if ($("#sidebar:has('.sidebar-active')"))
       $("#sidebar").toggleClass("sidebar-active");
-    })
+    });
+    // 设置收藏
+    function shoucang(sTitle, sURL) {
+      try {
+        window.external.addFavorite(sURL, sTitle);
+      } catch (e) {
+        try {
+          window.sidebar.addPanel(sTitle, sURL, "");
+        } catch (e) {
+          alert("加入收藏失败，请使用Ctrl+D进行添加");
+        }
+      }
+    }
 });
 
