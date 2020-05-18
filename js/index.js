6e935bfaef9b6e5719ab68c71c0ba79dd2e20744
lincucao <linzhixiong23@163.com>
2020-05-18 22:31:25 +0800

    update

--- js/index.js
+++ js/index.js
@@ -13,17 +13,6 @@ $(function(){
       if ($("#sidebar:has('.sidebar-active')"))
       $("#sidebar").toggleClass("sidebar-active");
     });
-    // 设置收藏
-    function shoucang(sTitle, sURL) {
-      try {
-        window.external.addFavorite(sURL, sTitle);
-      } catch (e) {
-        try {
-          window.sidebar.addPanel(sTitle, sURL, "");
-        } catch (e) {
-          alert("加入收藏失败，请使用Ctrl+D进行添加");
-        }
-      }
-    }
+
 });
 
